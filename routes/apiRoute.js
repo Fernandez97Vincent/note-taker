8646b50da65e20914bedb4ea96a58a763d844436
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-09-30 22:48:54 -0700

    Created a route to the notes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,3 +1,11 @@
 // create paths
 const router = require("express").Router();
 const store = require("../db/store");
+
+// make a route to notes
+router.getNotes().then((req, res) => {
+    // now get the notes from the store.js
+    store.getNotes().then((notes) => {
+        return resizeBy.json(notes)
+    }).catch((err) => res.status(500).json(err))
+});
